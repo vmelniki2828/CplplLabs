a2b31e73ee987ce9ed7dd63a4cb94fa1ff039a5e
vmelniki2828 <76560283+vmelniki2828@users.noreply.github.com>
2020-12-29 08:11:11 +0200

    Update main.cpp

--- Klab2/DynamicLibrary/main.cpp
+++ Klab2/DynamicLibrary/main.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main(void)
 {
 FileHandler fileHandler;
-	// Пошук студентів бригади за їх номером у списку
+
 	string oleksandr = fileHandler.getBrigadeMemberName(14);
 	string evgenii = fileHandler.getBrigadeMemberName(16);
 	string maksym = fileHandler.getBrigadeMemberName(20);
-	// Запис імен у файл бригади
+
 	fileHandler.writeStudentNameIntoBrigadeFile(oleksandr);
 	fileHandler.writeStudentNameIntoBrigadeFile(evgenii);
 	fileHandler.writeStudentNameIntoBrigadeFile(maksym);
